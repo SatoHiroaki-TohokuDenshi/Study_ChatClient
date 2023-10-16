f743f244b1ba5ea605f289525fa1bde118add6c5
央旺 佐藤 <220511@jc-21.jp>
2023-10-16 15:09:17 +0900

    大改修

--- Client.cpp
+++ Client.cpp
@@ -1,18 +1,3 @@
-/*
-WSAStartup()	// WinSock初期化
-socket()		// ソケット作成
-サーバのソケットアドレス情報をセット
-while(true)
-{
-	送信メッセージ入力
-	sendto();	// 送信
-	recvfrom();	// 受信
-	出力
-}
-closesocket()		// ソケット破棄
-WSACleanup()		// WinSock終了処理
-*/
-
 #include <iostream>
 #include <string>
 #include <WinSock2.h>
@@ -20,112 +5,80 @@ WSACleanup()		// WinSock終了処理
 
 #pragma comment( lib, "ws2_32.lib" )
 
+///// 定数宣言 /////
+const unsigned short SERVER_PORT = 8888;	//ポート番号
+const unsigned int MESSAGE_LENGTH = 1024;	//送受信の最大文字数
+
+using std::cin;
 using std::cout;
 using std::endl;
-
-// ポート番号
-const unsigned short SERVERPORT = 8888;
-
-// 送受信するメッセージの最大値
-const unsigned int MESSAGELENGTH = 1024;
-
-// プロトタイプ宣言
-int InitWinSock();
-//void SetServerIP(SOCKADDR_IN& sIP);
+using std::string;
 
 int main() {
-	cout << "You are Chat Client" << endl;
+	//結果格納用変数
+	int result = 0;
 
-	//初期化の結果を格納する変数
-	int ret;
+	//WinSock本体
+	WSADATA wsaData;
 
-	// WinSockの初期化
-	ret = InitWinSock();
-	if (ret != 0) {
-		cout << "WinSock Initializing Error";
+	//初期化
+	result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (result != 0) {
+		cout << "Error : WSAStartUp" << endl;
 		return -1;
 	}
-	cout << "Initialize WinSock is Success";
 
-	// ソケットディスクリプタ
+	//ソケットディスクリプタ
 	SOCKET sock;
 
-	// UDPソケット作成
+	//UDPソケット作成
 	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (sock < 0) {
-		cout << "Socket Creation Error" << endl;
+	if (sock == INVALID_SOCKET) {
+		cout << "Error : Socket" << endl;
 		return -1;
 	}
-	cout << "Create Socket is Success";
 
-	// 接続先サーバのIPアドレスを入力させる
-	std::string serverIpAddress;
-	std::cout << "Input Server IPv4 address :";
-	std::cin >> serverIpAddress;
 
-	// 接続先サーバのソケットアドレス情報格納
+	//通信先を入力
+	string serverIP = "";
+	cout << "Input Server IPv4 Address : ";
+	cin >> serverIP;
+
+	//通信先情報の格納
 	SOCKADDR_IN toAddr;
 	const int tolen = sizeof(toAddr);
 	memset(&toAddr, 0, sizeof(toAddr));
 	toAddr.sin_family = AF_INET;
-	toAddr.sin_port = htons(SERVERPORT);
-	ret = inet_pton(AF_INET, serverIpAddress.c_str(), &toAddr.sin_addr.s_addr);
-	if (ret < 0) {
-		cout << "Set Server IP Error" << endl;
+	toAddr.sin_port = htons(SERVER_PORT);
+	inet_pton(AF_INET, serverIP.c_str(), &toAddr.sin_addr.s_addr);
+
+	//メインループ
+	while (true) {
+		string message;
+		message.resize(MESSAGE_LENGTH);//上限値をサーバと合わせる
+		std::cout << "Input Message : ";
+		std::getline(cin, message);
+
+		result = sendto(sock, message.c_str(), MESSAGE_LENGTH, 0, (SOCKADDR*)&toAddr, tolen);
+		if (result == SOCKET_ERROR) {
+			cout << "Error : sendto" << endl;
+			return -1;
+		}
+	}
+
+	//ソケットの後処理
+	result = closesocket(sock);
+	if (result == SOCKET_ERROR) {
+		cout << "Error : closesocket" << endl;
 		return -1;
 	}
 
-	while (true) {
-		char message[MESSAGELENGTH];	// サーバ側とサイズを合わせること
-		std::cout << "Input message:";
-		std::cin >> message;
-
-		ret = sendto(sock, message, MESSAGELENGTH, 0, (SOCKADDR*)&toAddr, tolen);
-		if (ret == SOCKET_ERROR) {
-			/*
-				エラー処理
-			*/
-		}
-
-	}
-
-	ret = closesocket(sock);
-	if (ret == SOCKET_ERROR)
-	{
-		/*
-			エラー処理
-		*/
-	}
-
-	// WinSockの終了処理
-	ret = WSACleanup();
-	//if (ret == SOCKET_ERROR)
-	if (ret != 0)
-	{
-		/*
-			エラー処理
-		*/
+	//WinSockの後処理
+	result = WSACleanup();
+	if (result != 0) {
+		cout << "Error : WSACleanup" << endl;
+		return -1;
 	}
 
 	return 0;
 }
-
-int InitWinSock() {
-	//WinSockを表す構造体
-	WSADATA wsaData;
-	// WinSockの初期化  WinSock2.2
-	return(WSAStartup(MAKEWORD(2, 2), &wsaData));
-}
-
-//void SetServerIP(SOCKADDR_IN& sIP) {
-//	// 接続先サーバのIPアドレスを入力させる
-//	std::string serverIpAddress;
-//	std::cout << "Input Server IPv4 address :";
-//	std::cin >> serverIpAddress;
-//
-//	const int tolen = sizeof(sIP);
-//	memset(&sIP, 0, sizeof(sIP));
-//	sIP.sin_family = AF_INET;
-//	sIP.sin_port = htons(SERVERPORT);
-//	inet_pton(AF_INET, serverIpAddress.c_str(), &sIP.sin_addr.s_addr);
-//}
