adc1ca64445591ce508558e28ebac9c5832054e3
央旺 佐藤 <220511@jc-21.jp>
2023-10-17 11:29:09 +0900

    最低限

--- Client.cpp
+++ Client.cpp
@@ -28,16 +28,18 @@ int main() {
 		return -1;
 	}
 
+	cout << "Success : WinSock StartUp" << endl;
+
 	//ソケットディスクリプタ
 	SOCKET sock;
 
 	//UDPソケット作成
 	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if (sock == INVALID_SOCKET) {
-		cout << "Error : Socket" << endl;
+		cout << "Error : Create Socket" << endl;
 		return -1;
 	}
-
+	cout << "Success : Create Socket" << endl;
 
 	//通信先を入力
 	string serverIP = "";
@@ -50,19 +52,46 @@ int main() {
 	memset(&toAddr, 0, sizeof(toAddr));
 	toAddr.sin_family = AF_INET;
 	toAddr.sin_port = htons(SERVER_PORT);
-	inet_pton(AF_INET, serverIP.c_str(), &toAddr.sin_addr.s_addr);
+	result = inet_pton(AF_INET, serverIP.c_str(), &toAddr.sin_addr.s_addr);
+	if (result != 1) {
+		cout << "Error : Set Server IP Address" << endl;
+		return -1;
+	}
+	cout << "Success : Set Server IP Address" << endl;
 
+	cout << "Now Connecting..." << endl;
+	cout << "Connection opens now! Let's Communicate with Others!" << endl;
 	//メインループ
 	while (true) {
-		string message;
-		message.resize(MESSAGE_LENGTH);//上限値をサーバと合わせる
+		///// メッセージ送信 /////
+		//string message;
+		//message.resize(MESSAGE_LENGTH);//上限値をサーバと合わせる
+		//std::cout << "Input Message : ";
+		//std::getline(cin, message);
+		char message[MESSAGE_LENGTH];	//メッセージ
 		std::cout << "Input Message : ";
-		std::getline(cin, message);
+		cin >> message;
 
-		result = sendto(sock, message.c_str(), MESSAGE_LENGTH, 0, (SOCKADDR*)&toAddr, tolen);
+		result = sendto(sock, message, MESSAGE_LENGTH, 0, (SOCKADDR*)&toAddr, tolen);
 		if (result == SOCKET_ERROR) {
 			cout << "Error : sendto" << endl;
 			return -1;
+		}
+
+		///// メッセージ受信 /////
+		//上限値をサーバと合わせる
+		char buff[MESSAGE_LENGTH];	//受取用バッファ
+
+		SOCKADDR_IN fromAddr;
+		int fromlen = sizeof(fromAddr);
+		result = recvfrom(sock, buff, MESSAGE_LENGTH, 0, (SOCKADDR*)&fromAddr, &fromlen);
+		if (result == SOCKET_ERROR) {
+			cout << "Error : Recieve Message" << endl;
+			return -1;
+		}
+		else {
+			//表示
+			cout << "Receive Message : " << buff << endl;
 		}
 	}
 
